c743c9c0a5e4aeac52b532d80b25e9ab7e2c2932
Rafael Dias <raffa96dias@gmail.com>
2023-10-21 18:23:16 -0300

    up

---
+++ Estrutura Condicional/Exercício01/Exercício01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+
+namespace Exercício01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro:"); // Solicita ao usuário que digite um número
+            string input = Console.ReadLine(); // Lê a entrada do usuário e armazena como uma string
+
+            if (int.TryParse(input, out int inNum))
+            {
+                // Se a conversão for bem-sucedida
+                // Determina se o número é positivo ou negativo e armazena a mensagem apropriada em "mensagem"
+                string mensagem = inNum < 0 ? "NEGATIVO" : "POSITIVO";
+
+                // Exibe a mensagem, incluindo o valor e se é positivo ou negativo
+                Console.WriteLine("Você digitou " + inNum + " " + mensagem);
+            }
+            else
+            {
+                // Tratamento para entrada inválida
+                Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+            }
+
+        }
+    }
+}
