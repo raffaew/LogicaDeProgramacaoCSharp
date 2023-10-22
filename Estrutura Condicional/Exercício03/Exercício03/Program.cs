8c330079f4c5c3a994248b6ece052af1870bae6c
Rafael Dias <raffa96dias@gmail.com>
2023-10-22 16:05:22 -0300

    up EX03

---
+++ Estrutura Condicional/Exercício03/Exercício03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercício03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira dois valores inteiros!");
+            string[] input = (Console.ReadLine().Split(' '));
+
+            int num1 = int.Parse(input[0]);
+            int num2 = int.Parse(input[1]);
+
+            if(num1 % num2 == 0 || num2 % num1 == 0 )
+            {
+                Console.WriteLine($"Você digitou {num1} e {num2}. São multiplos!");
+            }else
+            {
+                Console.WriteLine($"Você digitou {num1} e {num2}. Não são multiplos!");
+            }
+            
+        }
+    }
+}
