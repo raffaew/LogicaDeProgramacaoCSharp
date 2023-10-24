dd832b706fa17d61c3229c0790fed2b116a5dc66
Rafael Dias <raffa96dias@gmail.com>
2023-10-24 20:07:28 -0300

    up

---
+++ Estrutura Repetitiva/ExercíciosFor/Exercício01/Exercício01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercício01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro para verificar quantos impares possui: ");
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input; i++)
+            {
+                // Se i / 2 resultar em resto da divisão = 0, i será um número imprar.
+                // A estrutura repete ate i ser igual ao número digitado pelo usuário.
+                if(i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Estrutura Repetitiva/ExercíciosFor/Exercício03/Exercício03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercício03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int dentro = 0;
+            int fora = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (input >= 10 && input <= 20){
+                    dentro++;
+                }
+                else {
+                    fora++;
+                }
+
+            }
+
+            Console.WriteLine(dentro + " in");
+            Console.WriteLine(fora + " out");
+
+        }
+    }
+}
